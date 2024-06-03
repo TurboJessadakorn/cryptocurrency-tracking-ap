8efa1894f4fd710067258a247322cc8dad59bf1e
TurboJessadakorn <105390501+TurboJessadakorn@users.noreply.github.com>
2024-06-04 01:28:17 +0700

    Setup nestjs moule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PortfolioModule } from './portfolio/portfolio.module';
+import { CryptoModule } from './crypto/crypto.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [PortfolioModule, CryptoModule, AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
