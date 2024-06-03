08bf69f498a05fb670874f403fd761817d162b62
TurboJessadakorn <105390501+TurboJessadakorn@users.noreply.github.com>
2024-06-04 01:57:52 +0700

    Connect nestjs with postgres db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,40 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PortfolioModule } from './portfolio/portfolio.module';
-import { CryptoModule } from './crypto/crypto.module';
 import { AuthModule } from './auth/auth.module';
+import { PortfolioModule } from './portfolio/portfolio.module';
 import { UserModule } from './user/user.module';
+import { CryptoModule } from './crypto/crypto.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Portfolio } from './portfolio/portfolio.entity';
+import { User } from './user/user.entity';
 
 @Module({
-  imports: [PortfolioModule, CryptoModule, AuthModule, UserModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule.forRoot({
+        isGlobal: true,
+        envFilePath: '.env',
+      })],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [ User, Portfolio ],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    PortfolioModule,
+    UserModule,
+    CryptoModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
