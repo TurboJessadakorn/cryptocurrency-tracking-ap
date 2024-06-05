a3704f0f1dd78048f73e5a8a63a5673cc5dc403e
TurboJessadakorn <105390501+TurboJessadakorn@users.noreply.github.com>
2024-06-05 14:32:26 +0700

    Get use information with jwt guard

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -6,14 +6,8 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  async login(@Request() req, @Res() res: Response) {
-    const token = await this.authService.login(req.body.username, req.body.password);
-    res.cookie('jwt', token.access_token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production', 
-      sameSite: 'strict',
-    });
-    return res.send({ message: 'Login successful' });
+  async login(@Request() req) {
+    return await this.authService.login(req.body.username, req.body.password);
   }
 
   @Post('logout')
--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,13 +1,25 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { CreateUserDto } from './user.dto';
 
-@Controller('users')
+@Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('register')
   async register(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  async getProfile(@Request() req) {
+    const user = await this.userService.findOne(req.user.username);
+    if (user) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
   }
 }
