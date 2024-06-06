b67412e101e5f6ead0fc747b75d7ae9271377fb7
TurboJessadakorn <105390501+TurboJessadakorn@users.noreply.github.com>
2024-06-06 20:05:01 +0700

    Edit get top crypto api

--- frontend/src/services/api/crypto.ts
+++ frontend/src/services/api/crypto.ts
@@ -8,6 +8,6 @@ export const getTopCryptos = async () => {
     return response.data;
   } catch (error) {
     console.error('Error fetching cryptocurrencies:', error);
-    throw error;
+    
   }
 };
